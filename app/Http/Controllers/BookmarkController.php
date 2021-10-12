5318ec998cda3e739e727d55c4cd58c0bd3c88a9
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-12 05:20:58 +0200

    #4 Added Bookmark model & controller

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\Weather;
+use App\Models\Bookmark;
+use App\Models\WeatherDownloader;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class HomeController extends Controller
+class BookmarkController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -23,6 +27,38 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $bookmarks = Bookmark::where('user_id', Auth::id())->get();
+        $allBookmarks = [];
+        foreach ($bookmarks as $bookmark) {
+            if ($weather = $this->getWeather($bookmark->location_slug)) {
+                $allBookmarks[] = $weather;
+            }
+        }
+
+        return view('bookmark', compact('allBookmarks'));
+    }
+
+    public function updateStatus(Request $request, string $slug)
+    {
+        $user = $request->user();
+        $bookmark = Bookmark::where('location_slug', $slug)->where('user_id', $user->id)->first();
+        if ($bookmark) {
+            $bookmark->delete();
+        } else {
+            Bookmark::create([
+                'location_slug' => $slug,
+                'user_id' => (int)$user->id
+            ]);
+        }
+
+        return redirect(route('search.show', ['city' => $slug]))
+            ->with('status', 'Update status for '. $slug);
+    }
+
+    private function getWeather(string $slug): Weather
+    {
+        $api = new WeatherDownloader();
+        $api->searchWeather($slug);
+        return $api->getWeather();
     }
 }
