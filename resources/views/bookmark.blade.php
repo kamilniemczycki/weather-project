61e261e1c6c8de00d883727491efa790cdd3f98d
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-12 05:24:37 +0200

    #3 Final

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -5,17 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                <div class="card-header">{{ __('Bookmarks') }}</div>
+                @forelse($allBookmarks as $bookmark)
+                <div class="found">
+                    <h1>{{ $bookmark->getCity() ?? 'undefined' }}</h1>
+                    <ul>
+                        <li>Country: {{ $bookmark->getCountry() ?? 'undefined' }}</li>
+                        <li>Weather: {{ $bookmark->getWeatherDesc() ?? 'undefined' }}</li>
+                        <li>{{ $bookmark->getTempC() ?? 'undefined' }} °C ({{ $bookmark->getTempF() ?? 'undefined' }} °F)</li>
+                    </ul>
+                    @if(\Illuminate\Support\Facades\Auth::check())
+                    <form action="{{ route('bookmark.update', ['slug' => Str::slug($bookmark->getCity())]) }}" method="POST">
+                        @csrf
+                        <input type="submit" value="Remove from bookmarks">
+                    </form>
                     @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
+                @empty
+                    Not found
+                @endforelse
             </div>
         </div>
     </div>
