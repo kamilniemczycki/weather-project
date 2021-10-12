f32affda4f014f8b1465f122f4d07fa3f6082ddb
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-10-12 03:27:21 +0200

    #2 Add support for API
    https://wttr.in/

---
+++ app/Interfaces/StatusCode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Interfaces;
+
+interface StatusCode
+{
+    /**
+     * @return int
+     */
+    public function statusCode(): int;
+}
---
+++ app/Interfaces/WeatherData.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces;
+
+interface WeatherData
+{
+    public function getWeather();
+}
