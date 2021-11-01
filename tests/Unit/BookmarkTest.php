15eb55bf69838a16adbe085e62582e3c63bf6c9e
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-11-01 15:19:45 +0100

    Added tests for Bookmark

--- tests/Unit/BookmarkTest.php
+++ tests/Unit/BookmarkTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class BookmarkTest extends TestCase
@@ -20,5 +21,32 @@ class BookmarkTest extends TestCase
     public function test_not_authorized_bookmark_update_status()
     {
         $this->post(route('bookmark.update', ['slug' => 'location']))->assertRedirect('/login');
+    }
+
+    public function test_add_bookmark()
+    {
+        /** @var User $user */
+        User::factory()->count(1)->make();
+        $user = User::first();
+        $user->bookmarks()->make([
+            'location_slug' => 'legnica'
+        ]);
+
+        $bookmark = $user->bookmarks()->first();
+        $this->assertEquals('legnica', $bookmark->location_slug);
+    }
+
+    public function test_remove_bookmark()
+    {
+        /** @var User $user */
+        User::factory()->count(1)->make();
+        $user = User::first();
+        $user->bookmarks()->make([
+            'location_slug' => 'legnica'
+        ]);
+
+        $bookmark = $user->bookmarks()->first();
+
+        $this->assertTrue($bookmark->delete());
     }
 }
