a465433e84f85276b0898af26ba50f9354e4e0e0
Kamil Niemczycki <contact@kamilcraft.com>
2022-02-02 00:33:21 +0100

    #20 Added new repository to Service Provider

--- app/Providers/WeatherAPIServiceProvider.php
+++ app/Providers/WeatherAPIServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Interfaces\Downloader\WeatherDownloader;
+use App\Repository\Interfaces\WeatherAPI as WeatherAPIInterface;
+use App\Repository\WeatherAPI;
 use Illuminate\Support\ServiceProvider;
 
 class WeatherAPIServiceProvider extends ServiceProvider
@@ -15,5 +17,6 @@ class WeatherAPIServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(WeatherDownloader::class, fn() => new \App\src\Downloader\WeatherDownloader());
+        $this->app->bind(WeatherAPIInterface::class, WeatherAPI::class);
     }
 }
