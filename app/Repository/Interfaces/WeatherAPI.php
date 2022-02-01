f6aeef5abea91439b2702580eea25a27ca7c9473
Kamil Niemczycki <contact@kamilcraft.com>
2022-02-02 00:31:49 +0100

    #20 Updated the interface with new methods

--- app/Repository/Interfaces/WeatherAPI.php
+++ app/Repository/Interfaces/WeatherAPI.php
@@ -5,4 +5,6 @@ namespace App\Repository\Interfaces;
 interface WeatherAPI
 {
     public function find(string $slug_name);
+    public function unSlugCity(string $slug_name): string;
+    public function statusCode(): int;
 }
